0c7f342f60a4a141022c474c035bf292b2c7e796
tunc2112 <sueruan012@gmail.com>
2020-04-27 14:16:49 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from vietnamese_syllable_regex import *
 
 if __name__ == '__main__':
 	for w in ["nghiêng", "khảo", "gì", "giếng", "quay", "quốc", "kao", "cào", "ngoéo", "nghành", "k", "gi", "qu"]:
-		print(split_vietnamese_syllable(w, True), split_vietnamese_syllable(w, False))
+		print(split_vietnamese_syllable(w))
